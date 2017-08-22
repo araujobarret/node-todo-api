e45efd55e82c8f85c2cf15351ef2bb02d5383ef6
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-08-22 13:36:15 -0300

    Mods to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ let mongoose = require('mongoose');
 let serverUri = "mongodb://root:killadmin@ds023523.mlab.com:23523/todoapp5523";
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(serverUri);
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(serverUri);
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
