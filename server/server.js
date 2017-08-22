f2996c5b567e1c4e4816d6c20db4e4ba9125096d
Carlos A A Barreto <araujobarret@gmail.com>
2017-08-21 22:45:23 -0300

    Todo get by id implemented

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -43,6 +44,27 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((doc) => {
+		res.send({todos: doc});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {		
+		if(!todo)
+			return res.status(404).send();
+		
+		res.send({todo});
+	}).catch((e) => res.status(400).send());	
+});
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
