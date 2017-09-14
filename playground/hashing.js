7ee07f173cac0ee0d561a6491ec3b1644bc59e4e
Carlos A A Barreto <araujobarret@gmail.com>
2017-09-13 23:39:06 -0300

    Password with random hash using decryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-	id: 10
-}
+let password = '123abc!';
 
-let token = jwt.sign(data, '123abc');
-let result = jwt.verify(token, '123abccc');
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
 
-console.log(token);
-console.log('Decoded', result);
+let hashedPassword = '$2a$10$qEzsir1CJzHgQveBQR7B1eaZP8nhZ61fshd7JUJKxIBw2Edph5eyW';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+	console.log('Result', result);
+});
+
+// let data = {
+// 	id: 10
+// }
+
+// let token = jwt.sign(data, '123abc');
+// let result = jwt.verify(token, '123abccc');
+
+// console.log(token);
+// console.log('Decoded', result);
 
 // jwt.verify();
 
