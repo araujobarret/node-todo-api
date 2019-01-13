7f5a05a286014021ce462a9df954f2af0faf2db7
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2019-01-13 21:33:59 -0200

    Deployment to heroku and password change for mlab, now is under a secure pattern, defined as env variables

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,21 @@
 let mongoose = require('mongoose');
 
-//let serverUri = "mongodb://root:killadmin@ds023523.mlab.com:23523/todoapp5523";
+let options = {
+	server: { socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}},
+	replset: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}}
+};
 
+// mLab connection
+let todoUser = process.env.TODO_USER;
+let todoPassword = process.env.TODO_PASSWORD;
+let mLabUri = `mongodb://${todoUser'}:${todoPassword}@ds023523.mlab.com:23523/todoapp5523`;
+
+mongoose.set('debug', true)
 mongoose.Promise = global.Promise;
-// mongoose.connect(serverUri);
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mLabUri, options);
+let conn = mongoose.connection;
+
+conn.on("error", console.error.bind(console, "conection error:"));
 
 module.exports = {
 	mongoose
