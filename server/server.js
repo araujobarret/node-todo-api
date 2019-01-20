9d13de841bd83bc011785be7a4855f6218bdf757
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2019-01-19 23:36:41 -0200

    Update server.js
    Enabled CORS and other stuff.

--- server/server.js
+++ server/server.js
@@ -12,6 +12,13 @@ let app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Expose-Headers', 'Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-auth');
+  next();
+});
 
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
